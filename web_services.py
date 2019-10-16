0c6361946c6cf77506c997031e85934f89f14975
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-10-16 13:52:27 -0700

    [addition] Added more download functionality

--- web_services.py
+++ web_services.py
@@ -12,24 +12,105 @@ class DataProvider():
         self.databases = self.db['databases']
 
     def create_project_downloadable(self, project_id):
-        html_files_to_create = []
+        project = self.projects.find_one({'_id': ObjectId(project_id)})
 
+        # Get html files
+        html_files_to_create = []
         for html_file in self.html_pages.find({'project_id': project_id}):
             html_files_to_create.append(html_file)
-
         print("HTML files to create:")
         print(html_files_to_create)
 
+        # Get databases
         databases_to_create = []
-
         for database in self.databases.find({'project_id': project_id}):
             databases_to_create.append(database)
-
         print("Databases to create:")
         print(databases_to_create)
 
+        # Directory creation
         print("\nCurrent working directory: {}".format(os.getcwd()))
+        output_dir = os.getcwd() + "/output"
+        print("Creating directory while on flask: {}".format(output_dir))
+        os.mkdir(output_dir)
 
-        print("Creating tmp directory while on flask")
+        # Create html files
+        self.create_html_files(html_files_to_create, project, output_dir)
 
-        os.mkdir("/Users/andreynovichkov/Desktop/Make-School/Term-1/Intensive/BuildABackend/tmp")
+        # Write app.py
+        self.write_app_dot_py(output_dir, html_files_to_create, project['name'], databases_to_create)
+
+    def create_html_files(self, html_files_to_create, project, output_dir):
+        # Create directory
+        html_files_path = output_dir + "/templates"
+        os.mkdir(html_files_path)
+
+        # Write base.html
+        self.write_base_file(html_files_path, project['name'])
+
+        # Write html files
+        for html_file in html_files_to_create:
+            self.write_html_file(html_files_path, html_file['name'])
+
+    def write_app_dot_py(self, output_dir, html_files, project_name, databases):
+        f = open("{}/app.py".format(output_dir), "w+", encoding="utf-8")
+
+        # import statements
+        f.write("from flask import Flask, render_template, request, redirect, url_for\n")
+
+        if len(databases) != 0:
+            f.write("from pymongo import MongoClient\nfrom bson.objectid import ObjectId\n")
+
+        # general initialization statements
+        f.write("\napp = Flask(__name__)\n\n")
+
+        # database initialization
+        if len(databases) != 0:
+            f.write("client = MongoClient()\n")
+            for database in databases:
+                f.write("{} = client[\'{}\']\n".format(database['name'].lower(), database['name']))
+                for collection in database['collections']:
+                    f.write("{} = {}[\'{}\']\n".format(collection, database['name'].lower(), collection))
+            f.write('\n\n')
+
+        # app.route statements
+        for html_file in html_files:
+            if html_file['http_verb'].lower() != 'post':
+                f.write("@app.route(\'{}\')\n".format(html_file['url']))
+            else:
+                f.write("@app.route(\'{}\', methods=['POST'])\n".format(html_file['url']))
+
+            f.write("def {}_function():\n".format(html_file['name']))
+            f.write("\t\"\"\"{}.\"\"\"\n\n".format(html_file['description']))
+            f.write("\tpass\n\n")
+
+        # if main statement
+        f.write("\nif __name__ == \"__main__\":\n\tapp.run(debug=True)")
+
+        f.close()
+
+    def write_base_file(self, html_files_path, project_name):
+        f = open("{}/base.html".format(html_files_path), "w+", encoding="utf-8")
+
+        f.write("<!DOCTYPE html>\n\n")
+        f.write("<html>\n")
+
+        f.write("<head>\n")
+        f.write("\t<title>{}</title>\n".format(project_name))
+        f.write("</head>\n\n")
+
+        f.write("<body>\n")
+        f.write("\t{% block content %} {% endblock %}\n")
+        f.write("</body>\n\n")
+
+        f.write("</html>")
+
+        f.close()
+
+    def write_html_file(self, html_files_path, html_filename):
+        f = open("{}/{}.html".format(html_files_path, html_filename), "w+", encoding="utf-8")
+
+        f.write("{% extends \"base.html\" %}\n\n")
+        f.write("{% block content %}\n\n")
+        f.write("<h1>This is {}.html!</h1>\n\n".format(html_filename))
+        f.write("{% endblock %}")
