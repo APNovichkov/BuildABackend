a9d50e4ff9468a174bbba0ad855e9255a281e8ae
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-10-17 13:01:36 -0700

    [addition] Added user functionality. Username supported

--- app.py
+++ app.py
@@ -35,17 +35,30 @@ def index():
 def show_login():
     return render_template('login.html')
 
-@app.route("/login/authorize")
+@app.route("/login/authorize", methods=['POST'])
 def login_user():
-    username = request.form.get('username')
+    user = {'username': request.form.get('username')}
+
+    user_id = None
+
+    if users.find_one({'username': user['username']}) is not None:
+        print("Username exists")
+        user_id = str(users.find_one({'username': user['username']})['_id'])
+    else:
+        print("Username does not exist")
+        print("Adding username: {}".format(user['username']))
+        user_id = str(users.insert_one(user).inserted_id)
+
+    return redirect(url_for("show_choose_project_page", user_id=user_id))
 
 
-@app.route("/builder/<project_id>")
-def show_builder(project_id):
+@app.route("/builder/<user_id>/<project_id>")
+def show_builder(user_id, project_id):
     """Show the builder template."""
 
     return render_template(
         "builder.html",
+        user_id=user_id,
         html_pages=html_pages.find({'project_id': project_id}),
         databases=databases.find({'project_id': project_id}),
         routes=routes.find({'project_id': project_id}),
@@ -54,16 +67,19 @@ def show_builder(project_id):
         num_databases=databases.find({'project_id': project_id}).count(),
         num_routes=routes.find({'project_id': project_id}).count())
 
-@app.route("/choose-project")
-def show_choose_project_page():
+@app.route("/choose-project/<user_id>")
+def show_choose_project_page(user_id):
     """Show choose project page or create new project."""
+
+    print("This is the user_id from show_choose_project_page: {}".format(user_id))
 
     return render_template(
         "choose_project.html",
-        projects=projects.find())
+        projects=projects.find({'user_id': user_id}),
+        user_id=user_id)
 
-@app.route("/builder/delete/<project_id>")
-def remove_project(project_id):
+@app.route("/builder/delete/<user_id>/<project_id>")
+def remove_project(user_id, project_id):
     """Delete project based on project_id thats passted in."""
 
     html_pages.delete_many({'project_id': project_id})
@@ -72,31 +88,34 @@ def remove_project(project_id):
 
     projects.delete_one({'_id': ObjectId(project_id)})
 
-    return redirect(url_for("show_choose_project_page"))
+    return redirect(url_for("show_choose_project_page", user_id=user_id))
 
-@app.route("/builder/new")
-def show_new_project_page():
+@app.route("/builder/<user_id>/new")
+def show_new_project_page(user_id):
     """Show the NEW PROJECT form/page."""
 
-    return render_template("new_project.html")
+    return render_template("new_project.html", user_id=user_id)
 
-@app.route("/builder/new", methods=['POST'])
-def create_new_project():
+@app.route("/builder/<user_id>/new", methods=['POST'])
+def create_new_project(user_id):
     """Create NEW PROJECT."""
+
+    print("This is the user_id from create_new_project: {}".format(user_id))
 
     project = {
         'name': request.form.get("project-name"),
+        'user_id': user_id,
         'time_created': datetime.now().strftime("%c"),
         'last_modified': datetime.now().strftime("%c")
     }
 
     project_id = projects.insert_one(project).inserted_id
 
-    return redirect(url_for("show_builder", project_id=project_id))
+    return redirect(url_for("show_builder", user_id=user_id, project_id=project_id))
 
 # CRUD for HTML pages
-@app.route("/builder/add-html", methods=['POST'])
-def add_html_page():
+@app.route("/builder/<user_id>/add-html", methods=['POST'])
+def add_html_page(user_id):
     """Add new HTML page to builder."""
 
     html_page = {
@@ -111,11 +130,11 @@ def add_html_page():
     projects.find_one_and_update({'_id': ObjectId(html_page['project_id'])}, {'$set': {'last_modified': datetime.now().strftime("%c")}})
 
     html_pages.insert_one(html_page)
-    return redirect(url_for("show_builder", project_id=html_page['project_id']))
+    return redirect(url_for("show_builder", user_id=user_id, project_id=html_page['project_id']))
 
 
-@app.route("/builder/delete-html-page/<html_page_id>")
-def remove_html_page(html_page_id):
+@app.route("/builder/<user_id>/delete-html-page/<html_page_id>")
+def remove_html_page(user_id, html_page_id):
     """Remove HTML page from builder by the page_id given."""
 
     project_id = html_pages.find_one({'_id': ObjectId(html_page_id)})['project_id']
@@ -124,11 +143,11 @@ def remove_html_page(html_page_id):
 
     projects.find_one_and_update({'_id': ObjectId(project_id)}, {'$set': {'last_modified': datetime.now().strftime("%c")}})
 
-    return redirect(url_for("show_builder", project_id=project_id))
+    return redirect(url_for("show_builder", user_id=user_id, project_id=project_id))
 
 # CRUD for DATABASES
-@app.route("/builder/add-database", methods=['POST'])
-def add_database():
+@app.route("/builder/<user_id>/add-database", methods=['POST'])
+def add_database(user_id):
     """Add new database to builder."""
 
     collections = request.form.get('collections').split()
@@ -143,10 +162,10 @@ def add_database():
 
     databases.insert_one(database)
 
-    return redirect(url_for("show_builder", project_id=database['project_id']))
+    return redirect(url_for("show_builder", user_id=user_id, project_id=database['project_id']))
 
-@app.route("/builder/delete-database/<database_id>")
-def remove_database(database_id):
+@app.route("/builder/<user_id>/delete-database/<database_id>")
+def remove_database(user_id, database_id):
     """Remove Database from builder by the database_id given."""
 
     project_id = databases.find_one({'_id': ObjectId(database_id)})['project_id']
@@ -155,12 +174,12 @@ def remove_database(database_id):
 
     databases.delete_one({'_id': ObjectId(database_id)})
 
-    return redirect(url_for("show_builder", project_id=project_id))
+    return redirect(url_for("show_builder", user_id=user_id, project_id=project_id))
 
 
 # CRUD for ROUTES
-@app.route("/builder/add-route", methods=['POST'])
-def add_route():
+@app.route("/builder/<user_id>/add-route", methods=['POST'])
+def add_route(user_id):
     """Add new route to builder."""
 
     route = {
@@ -176,10 +195,10 @@ def add_route():
 
     routes.insert_one(route)
 
-    return redirect(url_for("show_builder", project_id=route['project_id']))
+    return redirect(url_for("show_builder", user_id=user_id, project_id=route['project_id']))
 
-@app.route("/builder/delete_route/<route_id>", methods=['POST'])
-def remove_route(route_id):
+@app.route("/builder/<user_id>/delete_route/<route_id>", methods=['POST'])
+def remove_route(user_id, route_id):
 
     project_id = routes.find_one({'_id': ObjectId(route_id)})['project_id']
 
@@ -187,7 +206,7 @@ def remove_route(route_id):
 
     routes.delete_one({'_id': ObjectId(route_id)})
 
-    return redirect(url_for("show_builder", project_id=project_id))
+    return redirect(url_for("show_builder", user_id=user_id, project_id=project_id))
 
 
 @app.route("/builder/download/<project_id>")
